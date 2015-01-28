81739b77080ebba0a9972425791a3643fee249f4
idanbeck <idanbeck@incidenttech.com>
2015-01-27 18:31:51 -0800

    removing stop gap includes from before

--- AMON/AMONPHY.c
+++ AMON/AMONPHY.c
@@ -4,13 +4,6 @@
 #include "AMONPHY.h"
 #include "AMONNet.h"	// Needed to funnel bytes to HandleAMONByte
 
-// TODO: Remove these - this should be entirely application side
-// The application will register the call backs for the actual PHY functionality
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-#include "inc/lm4f230h5qr.h"
-#include "driverlib/rom.h"
-
 AMON_PHY_STATE g_AMONLinkPhys[NUM_LINKS];
 
 cbSendByteOnLink g_PHYSendByteCallbacks[NUM_LINKS];
